52b733ac982faebaa7dcf8f1d807002410e96088
Jan <pi@raspberrypi>
2022-05-22 19:31:59 +0200

    wieder ohne Plot! Es brauch 500ms jedesmal, wenn geplottet wird. Dass ist zu langsam! Eine möglichkeit wäre multithreading...

--- mainwindow.py
+++ mainwindow.py
@@ -23,7 +23,7 @@ from PyQt5.QtWidgets import QDialog, QApplication, QStackedWidget, QWidget, QVBo
 from PyQt5.QtCore import QTimer
 
 
-
+"""
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
@@ -31,7 +31,7 @@ class MplCanvas(FigureCanvas):
         super(MplCanvas, self).__init__(self.fig)
         self.fig.tight_layout()
         self.axes.set_ylabel('T / [°C]')
-
+"""
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -57,23 +57,31 @@ class MainWindow(QWidget):
         self.qTimer.start()
         self.saveBtn.clicked.connect(self.writeData)
         self.stopBtn.clicked.connect(self.stopExit)
-        self.canvas = MplCanvas(self)
-        l = QVBoxLayout(self.plot2Widget)
-        l.addWidget(self.canvas)
+        #self.canvas = MplCanvas(self)
+        #l = QVBoxLayout(self.plot2Widget)
+        #l.addWidget(self.canvas)
         self.temp1 = readTempData.ReadTempData()
         #self.dummiSensor = rSD.ReadTemperature()
-
+    """
     def plotUpdate(self):
         try:
-            self.plotdata = np.vstack((self.plotdata, float(self.valueT3.text().split(' ')[0])))[-100:]
-            self.plotdataT4 = np.vstack((self.plotdataT4, float(self.valueT4.text().split(' ')[0])))[-100:]
+            #self.plotdataT1 = np.vstack((self.plotdataT1, float(self.valueT1.text().split(' ')[0])))[-5:]
+            #self.plotdataT2 = np.vstack((self.plotdataT2, float(self.valueT2.text().split(' ')[0])))[-5:]
+            self.plotdataT3 = np.vstack((self.plotdataT3, float(self.valueT3.text().split(' ')[0])))[-5:]
+            self.plotdataT4 = np.vstack((self.plotdataT4, float(self.valueT4.text().split(' ')[0])))[-5:]
+            self.plotdataP1 = np.vstack((self.plotdataP1, float(self.valueP1.text().split(' ')[0])))[-5:]
+            self.plotdataP2 = np.vstack((self.plotdataP2, float(self.valueP2.text().split(' ')[0])))[-5:]
 
         except:
             # first data point
             print('except')
-            self.plotdata = np.array([[(float(self.valueT3.text().split(' ')[0]))]])
+            #self.plotdataT1 = np.array([[(float(self.valueT1.text().split(' ')[0]))]])
+            #self.plotdataT2 = np.array([[(float(self.valueT2.text().split(' ')[0]))]])
+            self.plotdataT3 = np.array([[(float(self.valueT3.text().split(' ')[0]))]])
             self.plotdataT4 = np.array([[(float(self.valueT4.text().split(' ')[0]))]])
-        self.ydata = self.plotdata
+            self.plotdataP1 = np.array([[(float(self.valueP1.text().split(' ')[0]))]])
+            self.plotdataP2 = np.array([[(float(self.valueP2.text().split(' ')[0]))]])
+        #self.ydata = self.plotdata
 
         self.canvas.axes.clear()
         self.canvas.axes.set_facecolor((0, 0, 0))
@@ -81,12 +89,15 @@ class MainWindow(QWidget):
         start, end = self.canvas.axes.get_ylim()
         self.canvas.axes.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
         self.canvas.axes.set_autoscale_on(True)  #set_ylim( ymin=0, ymax=100)
-
-        self.canvas.axes.plot(self.ydata, color=(0,1,0.29))
+        #self.canvas.axes.plot(self.plotdataT1, color=(0,1,0.29))
+        #self.canvas.axes.plot(self.plotdataT2, color=(0,1,0))
+        self.canvas.axes.plot(self.plotdataT3, color=(1,0,0.29))
         self.canvas.axes.plot(self.plotdataT4, color=(1,0,0))
+        self.canvas.axes.plot(self.plotdataP1, color=(0,1,1))
+        self.canvas.axes.plot(self.plotdataP2, color=(0,1,1))
         self.canvas.draw()
         self.canvas.flush_events()
-
+        """
 
     def load_ui(self):
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
@@ -125,32 +136,17 @@ class MainWindow(QWidget):
 
 
         #self.step += 1
-        self.step_plot += 1
+        #self.step_plot += 1
         """            if self.checkSaveP1.isChecked() is True:
             self.d['P1 / [Bar]'] = []
             self.writeP1.setVisible(True)
-        """
+
         if self.step_plot >10:
             self.plotUpdate()
             self.step_plot = 0
 
         """
-        if self.step > len(self.sensorList) - 1:
-            self.step = 0
-            #self.plotUpdate()
-            if self.saving is True:
-                _now = time.time()
-                t = time.strftime('%Y%m%d %X', time.localtime(_now)) + ':' + str('%.3f' % _now).split('.')[1]
-                self.d['Time'].append(t)
-                if self.checkSaveT1.isChecked() is True:
-                    self.d['T1 / [°C]'].append(self.valueT1.text().split(' ')[0])
-                if self.checkSaveT2.isChecked() is True:
-                    self.d['T2 / [°C]'].append(self.valueT2.text().split(' ')[0])
-                if self.checkSaveT3.isChecked() is True:
-                    self.d['T3 / [°C]'].append(self.valueT3.text().split(' ')[0])
-                if self.checkSaveT4.isChecked() is True:
-                    self.d['T4 / [°C]'].append(self.valueT4.text().split(' ')[0])
-          """
+
 
     def writeData(self):
         if self.saving is False or self.saving is None:
