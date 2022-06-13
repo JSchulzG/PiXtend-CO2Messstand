e0f17c20215e6daf3db661fba8bdb726f5499ac7
Schulz-Gericke <jan.schulz-gericke@gmx.net>
2022-06-13 21:37:29 +0200

    als script welches als arg einen path bekommt

--- plotData.py
+++ plotData.py
@@ -1,20 +1,38 @@
+import csv
+import glob
 import matplotlib.pyplot as plt
 import pandas
 import sys
 import datetime
 
-path = '/home/jan/Dokumente/20220612_daten/20220612145925_data.csv'
-#path = sys.argv[1]
-print(path)
-dataFrame = pandas.read_csv(path, header=1)
-fig, ax = plt.subplots()
-timeLine = [(datetime.datetime.strptime(time, '%H:%M:%S.%f') - datetime.datetime.strptime(dataFrame['Time'][0], '%H:%M:%S.%f'))/datetime.timedelta(seconds=1) for time in dataFrame['Time'] ]
-for key in dataFrame.keys():
-    if key != 'Unnamed: 0' and key != 'Time':
-        ax.plot(timeLine, dataFrame[key].values, linewidth=1.5, label=key)
-print(datetime.datetime.strptime(dataFrame['Time'][2], '%H:%M:%S.%f')- datetime.datetime.strptime(dataFrame['Time'][1], '%H:%M:%S.%f'))
-ax.legend()
-#ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
- #      ylim=(0, 8), yticks=np.arange(1, 8))
-print(dataFrame['T1/[°C]'].values)
-plt.show()
+from pyparsing import empty
+
+#path = '/home/jan/Dokumente/20220612_daten/20220612145925_data.csv'
+
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    print(path)
+    file = open(path)
+    startData = 0
+    title = ''
+    while True:
+        line = file.readline()
+        if 'Time,Pos/[cm]' in line:
+            break
+        if line != '\n':
+            title += line
+            startData += 1
+    print(title)
+    dataFrame = pandas.read_csv(path, header=startData)
+    fig, ax = plt.subplots()
+    timeLine = [(datetime.datetime.strptime(time, '%H:%M:%S.%f') - datetime.datetime.strptime(dataFrame['Time'][0], '%H:%M:%S.%f'))/datetime.timedelta(seconds=1) for time in dataFrame['Time'] ]
+    for key in dataFrame.keys():
+        if key != 'Unnamed: 0' and key != 'Time':
+            ax.plot(timeLine, dataFrame[key].values, linewidth=1.5, label=key)
+    ax.legend()
+    plt.title(title)
+    #plt.show()
+    pathImg = path.split('.')[0]+'.png'
+    plt.savefig(pathImg)
