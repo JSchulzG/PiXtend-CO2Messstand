affbef836b5df73d0df849e370d44dda2e012026
Schulz-Gericke <jan.schulz-gericke@gmx.net>
2022-06-14 14:57:54 +0200

    merge plotData.py from pcBranch

--- plotData.py
+++ plotData.py
@@ -1,15 +1,19 @@
-import csv
-import glob
+
+from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 import pandas
 import sys
 import datetime
-
+import numpy as np
 from pyparsing import empty
 
 
-def plot(path, show=False, save=True):
+def plot(path, show=False, save=True, cycle=False):
     print(path)
+    if '.csv' != path[-4:]:
+        print('bitte eine csv Datei übergeben!')
+        return
+
     file = open(path)
     startData = 0
     title = ''
@@ -23,19 +27,54 @@ def plot(path, show=False, save=True):
     print(title)
     dataFrame = pandas.read_csv(path, header=startData)
     fig, (axT, axP, axS) = plt.subplots(3, sharex=True, gridspec_kw={'hspace': 0})
-    timeLine = [(datetime.datetime.strptime(time, '%H:%M:%S.%f') - datetime.datetime.strptime(dataFrame['Time'][0], '%H:%M:%S.%f'))/datetime.timedelta(seconds=1) for time in dataFrame['Time'] ]
+    timeLine = [(datetime.datetime.strptime(time, '%H:%M:%S.%f') - 
+                 datetime.datetime.strptime(dataFrame['Time'][0], 
+                 '%H:%M:%S.%f'))/datetime.timedelta(seconds=1) for time in dataFrame['Time'] ]
+    dataFrame['Time'] = timeLine
+    dataFrame.set_index('Time', inplace=True)
     for key in dataFrame.keys():
         if key != 'Unnamed: 0' and key != 'Time':
             if '°C' in key:
-                axT.plot(timeLine, dataFrame[key].values, linewidth=1.5, label=key)
+                axT.plot(dataFrame[key], linewidth=1.5, label=key)
                 axT.set(ylabel='°C')
             if 'Bar' in key:
-                axP.plot(timeLine, dataFrame[key].values, linewidth=1.5, label=key)
+                axP.plot(dataFrame[key], linewidth=1.5, label=key)
                 axP.set(ylabel='Bar')
             if 'cm' in key:
-                axS.plot(timeLine, dataFrame[key].values, linewidth=1.5, label=key)
+                axS.plot(dataFrame[key], linewidth=1.5, label=key)
                 axS.set(ylabel='cm')
-    
+    if cycle == True:
+        # Bestimmung der lokalen Minima 
+        dataFrame['min'] = dataFrame.iloc[argrelextrema(dataFrame['Pos/[cm]'].values, np.less_equal,
+                        order=2000)[0]]['Pos/[cm]']
+        #axS.scatter(dataFrame.index, dataFrame['min'], c='r')
+        dataFrame['max'] = dataFrame.iloc[argrelextrema(dataFrame['Pos/[cm]'].values, np.greater_equal,
+                        order=2000)[0]]['Pos/[cm]']
+        #axS.scatter(dataFrame.index, dataFrame['max'], c='g')
+        # Bestimmung der Zykluszeit von erstem lokalen Min zu dem nächsten.
+        # D.h. Zeit die der Kolben braucht um von links nach rechts und zurück zu fahren.
+        # Daraus liesse sich die Leistung berechnen.
+        deltaTime = 0.0
+        startTime = 0.0
+        dataFrame['startZyklus'] = np.nan
+        for time in dataFrame.index:
+            if dataFrame['min'][time] >= 0 and startTime == 0.0:
+                #print(time)
+                startTime = time
+                dataFrame.at[time, 'startZyklus'] = dataFrame['min'][time]
+            elif dataFrame['min'][time] >= 0 and deltaTime < 40.0:
+                deltaTime = time - startTime
+            elif dataFrame['min'][time] >= 0 and deltaTime >40.0:
+                deltaTime = time - startTime
+                print('Zyklus Zeit: %.3f' %deltaTime)
+                dataFrame.at[time, 'startZyklus'] = dataFrame['min'][time]
+                startTime = time
+                deltaTime = 0.0
+        axS.scatter(dataFrame.index, dataFrame['startZyklus'], c='r')
+        for time in dataFrame.index:
+            if dataFrame['startZyklus'][time] != np.nan:
+                axS.annotate('%.1f'%time, (time-40, dataFrame['startZyklus'][time]))
+        
     axT.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     axP.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     axS.legend(loc='center left', bbox_to_anchor=(1, 0.5))
@@ -51,4 +90,5 @@ def plot(path, show=False, save=True):
 
 if __name__ == '__main__':
     path = sys.argv[1]
-    plot(path, show=True, save=False)
+    
+    plot(path, show=True, save=False, cycle=True)
