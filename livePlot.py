f2941e7c907f45737b50a8ccbca79a671161bf7e
Jan SG <jan.schulz-gericke@gmx.net>
2022-07-20 14:47:38 +0200

    Create Subplots for Temp, Pressure and Position
    The plotting is getting slow again when the amount of datapoints
    is rising. But the messurement cycle is still at 50ms.

--- livePlot.py
+++ livePlot.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 plt.style.use('fivethirtyeight')
-
+#fig, (axT, axP, axPos) = plt.subplots(3)
+fig = plt.figure()
+gs = fig.add_gridspec(3, hspace=0)
+axT, axP, axPos = gs.subplots(sharex=True)
+fig.suptitle('Live Plot Daten')
 x_vals = []
 y_vals = []
 
@@ -15,16 +19,25 @@ index = count()
 
 def animate(i):
     data = pd.read_csv('data.csv')
-    x = data['time']
-    y1 = data['T1']
-    y2 = data['T2']
-
+    time = data['time']
+    t1 = data['T1']
+    t2 = data['T2']
+    t3 = data['T3']
+    t4 = data['T4']
+    p1 = data['P1']
+    p2 = data['P2']
+    pos = data['Pos']
+    # print(time)
     plt.cla()
 
-    plt.plot(x, y1, label='Channel 1')
-    plt.plot(x, y2, label='Channel 2')
-
-    plt.legend(loc='upper left')
+    #axT.plot(time, t1, color='red')
+    axT.plot(time, t2, color='red')
+    #axT.plot(time, t3, color='green')
+    axT.plot(time, t4, color='green')
+    axP.plot(time, p1, color='red')
+    axT.label_outer()
+    axP.label_outer()
+    axPos.label_outer()
     plt.tight_layout()
 
 
