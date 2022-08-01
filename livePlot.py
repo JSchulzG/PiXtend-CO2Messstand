7931ad3a92f04653d7ab41d3df160b35dc6a05f3
Jan SG <jan.schulz-gericke@gmx.net>
2022-08-01 17:26:55 +0200

    Referece plots and update them
    This method is faster and all lines are for a couple
    of minutes fast enough.
    
    ToDo: styling of the plot

--- livePlot.py
+++ livePlot.py
@@ -1,3 +1,4 @@
+
 import csv
 import os
 from pathlib import Path
@@ -21,6 +22,15 @@ start = True
 index = count()
 fieldnames = ['time', 'T1', 'T2', 'T3', 'T4', 'P1', 'P2', 'Pos']
 lengthPlot = 45.0
+_plot_ref = {}
+time = []
+t1 = []
+t2 = []
+t3 =[]
+t4 = []
+p1 = []
+p2 = []
+pos = []
 
 def animate(i):
     if Path('data.csv').is_file():
@@ -37,38 +47,62 @@ def animate(i):
         os.remove('data2.csv')
 
     try:
-        time = data['time']
+        time.append(data['time'].mean())
     except:
         print('data vault')
         return
-    t1 = data['T1']
-    t2 = data['T2']
-    t3 = data['T3']
-    t4 = data['T4']
-    p1 = data['P1']
-    p2 = data['P2']
-    pos = data['Pos']
+
+    t1.append(round(data['T1'].mean(), 1))
+    t2.append(round(data['T2'].mean(), 1))
+    t3.append(round(data['T3'].mean(), 1))
+    t4.append(round(data['T4'].mean(), 1))
+    p1.append(round(data['P1'].mean(), 2))
+    p2.append(round(data['P2'].mean(), 2))
+    pos.append(round(data['Pos'].mean(), 0))
     #print(time)
     #plt.cla()
     #if start == True:
     #    start_time = time[0]
     #    stat = False
     #    print(start_time)
-    if time[1] > lengthPlot:
-        axT.set_xlim(time[1]-lengthPlot, time[1])
-        axP.set_xlim(time[1]-lengthPlot, time[1])
-        axPos.set_xlim(time[1]-lengthPlot, time[1])
-
-    axT.plot(time, t1, color='red', marker='.', label='T1')
-    axT.plot(time, t2, color='red', marker='.')
-    axT.plot(time, t3, color='green', marker='.')
-    axT.plot(time, t4, color='green', marker='.')
-    axP.plot(time, p1, color='red', marker='.')
-    axP.plot(time, p2, color='green', marker='.')
-    axPos.plot(time, pos, color='blue', marker='.', linestyle='None')
-    axT.label_outer()
-    axP.label_outer()
-    axPos.label_outer()
+    #if time[1] > lengthPlot:
+    #    axT.set_xlim(time[1]-lengthPlot, time[1])
+    #    axP.set_xlim(time[1]-lengthPlot, time[1])
+    #    axPos.set_xlim(time[1]-lengthPlot, time[1])
+    #axT.plot.clr()
+    if len(_plot_ref) == 0:
+        _plot_ref['T1'] = axT.plot(time, t1, color='red', marker='.', label='T1')[0]
+        _plot_ref['T2'] = axT.plot(time, t2, color='red', marker='.')[0]
+        _plot_ref['T3'] = axT.plot(time, t3, color='green', marker='.')[0]
+        _plot_ref['T4'] = axT.plot(time, t4, color='green', marker='.')[0]
+        _plot_ref['P1'] = axP.plot(time, p1, color='red', marker='.')[0]
+        _plot_ref['P2'] = axP.plot(time, p2, color='green', marker='.')[0]
+        _plot_ref['Pos'] = axPos.plot(time, pos, color='blue', marker='.')[0]
+        axT.set_ylim(10,50)
+        axP.set_ylim(30,90)
+        axPos.set_ylim(0,20)
+    else:
+        #axT.autoscale()
+        axT.set_xlim(time[0], time[-1])
+        #axT.set_ylim()
+        axP.set_xlim(time[0], time[-1])
+        axPos.set_xlim(time[0], time[-1])
+        _plot_ref['T1'].set_data(time, t1)
+        _plot_ref['T2'].set_data(time, t2)
+        _plot_ref['T3'].set_data(time, t3)
+        _plot_ref['T4'].set_data(time, t4)
+        _plot_ref['P1'].set_data(time, p1)
+        _plot_ref['P2'].set_data(time, p2)
+        _plot_ref['Pos'].set_data(time, pos)
+    #axT.plot(time, t2, color='red', marker='.')
+    #axT.plot(time, t3, color='green', marker='.')
+    #axT.plot(time, t4, color='green', marker='.')
+    #axP.plot(time, p1, color='red', marker='.')
+    #axP.plot(time, p2, color='green', marker='.', linestyle='None')
+    #axPos.plot(time, pos, color='blue', marker='.', linestyle='None')
+    #axT.label_outer()
+    #axP.label_outer()
+    #axPos.label_outer()
     plt.tight_layout()
 
 
